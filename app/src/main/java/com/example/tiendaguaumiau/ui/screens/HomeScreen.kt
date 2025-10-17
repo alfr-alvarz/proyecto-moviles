b2b8dab9158c958bba9660eaf5f055aa622861fa
unknown <Cetecom@SJAPCA03LC0715.duoc.acad>
2025-10-17 13:36:25 -0300

    feat: Navegacion implementada

--- app/src/main/java/com/example/tiendaguaumiau/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/tiendaguaumiau/ui/screens/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
@@ -21,12 +22,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.tiendaguaumiau.navigation.Screen
 import com.example.tiendaguaumiau.viewmodel.MainViewModel
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navController: NavController,

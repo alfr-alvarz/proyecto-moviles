af8d604b65d36434e1d004f73466158d530bb827
Alfredo <alf.alvarezr@duocuc.cl>
2025-11-27 20:04:29 -0300

    refactor: AppNavigation movido

--- app/src/main/java/com/example/tiendaguaumiau/MainActivity.kt
+++ app/src/main/java/com/example/tiendaguaumiau/MainActivity.kt
@@ -3,37 +3,22 @@ package com.example.tiendaguaumiau
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-//import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.LaunchedEffect
-
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
+import com.example.tiendaguaumiau.navigation.AppNavigation
 import com.example.tiendaguaumiau.navigation.NavigationEvent
-import com.example.tiendaguaumiau.navigation.Screen
-import com.example.tiendaguaumiau.ui.screens.HomeScreen
-import com.example.tiendaguaumiau.ui.screens.ProfileScreen
-import com.example.tiendaguaumiau.ui.screens.SettingsScreen
-import com.example.tiendaguaumiau.ui.screens.RegisterScreen
-import com.example.tiendaguaumiau.ui.screens.LoginScreen
-
-
-
-
 import com.example.tiendaguaumiau.view.theme.TiendaGuauMiauTheme
 import com.example.tiendaguaumiau.viewmodel.MainViewModel
 import kotlinx.coroutines.flow.collectLatest
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContent {
             TiendaGuauMiauTheme {
 
@@ -64,37 +49,13 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    NavHost(
+                    AppNavigation(
                         navController = navController,
-                        startDestination = Screen.Login.route,
+                        viewModel = viewModel,
                         modifier = Modifier.padding(innerPadding)
-                    ) {
-                        composable(route = Screen.Home.route){
-                            HomeScreen(navController = navController,viewModel = viewModel)
-                        }
-                        composable(route= Screen.Profile.route){
-                            ProfileScreen(navController = navController, viewModel=viewModel)
-                        }
-                        composable(route = Screen.Settings.route){
-                            SettingsScreen(navController = navController, viewModel = viewModel)
-                        }
-                        composable(route = Screen.Register.route){
-                            RegisterScreen(navController = navController)
-                        }
-                        composable(route = Screen.Login.route){
-                            LoginScreen(navController = navController, viewModel = viewModel)
-                        }
-                    }
+                    )
                 }
             }
         }
     }
 }
-
-/*
-@Preview(name="Compact", widthDp = 360, heightDp = 800)
-@Composable
-fun PreviewCompact(){
-    HomeScreenCompacta()
-}
-*/
---
+++ app/src/main/java/com/example/tiendaguaumiau/navigation/AppNavigation.kt
@@ -0,0 +1,42 @@
+package com.example.tiendaguaumiau.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.tiendaguaumiau.ui.screens.HomeScreen
+import com.example.tiendaguaumiau.ui.screens.LoginScreen
+import com.example.tiendaguaumiau.ui.screens.ProfileScreen
+import com.example.tiendaguaumiau.ui.screens.RegisterScreen
+import com.example.tiendaguaumiau.ui.screens.SettingsScreen
+import com.example.tiendaguaumiau.viewmodel.MainViewModel
+
+@Composable
+fun AppNavigation(
+    navController: NavHostController,
+    viewModel: MainViewModel,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Login.route,
+        modifier = modifier
+    ) {
+        composable(route = Screen.Home.route) {
+            HomeScreen(navController = navController, viewModel = viewModel)
+        }
+        composable(route = Screen.Profile.route) {
+            ProfileScreen(navController = navController, viewModel = viewModel)
+        }
+        composable(route = Screen.Settings.route) {
+            SettingsScreen(navController = navController, viewModel = viewModel)
+        }
+        composable(route = Screen.Register.route) {
+            RegisterScreen(navController = navController)
+        }
+        composable(route = Screen.Login.route) {
+            LoginScreen(navController = navController, viewModel = viewModel)
+        }
+    }
+}
